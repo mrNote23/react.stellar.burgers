d0080296b63b48193140b98d7825baad4e209a5f
AndreyS <suvorov.av93@gmail.com>
2023-05-25 11:12:08 +0300

    to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/app/app";
+import App from "./components/app/app.js";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
