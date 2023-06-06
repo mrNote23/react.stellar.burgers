fdfe9f43ab60199cd7e85977889daac0e59f9d15
Andrey <suvorov.av93@gmail.com>
2023-06-06 13:17:26 +0300

    fix: clear burger only if createOrder success

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,103 +1,81 @@
-import { useEffect, useMemo, useRef } from "react";
-import { useDrop } from "react-dnd";
-import { nanoid } from "nanoid";
-import { TBurger, TIngredient } from "../../types";
-import {
-  ConstructorElement,
-  CurrencyIcon,
-  Button,
-} from "@ya.praktikum/react-developer-burger-ui-components";
-import Modal from "../modal/modal";
-import OrderDetails from "../order-details/order-details";
-import styles from "./burger-constructor.module.css";
-import BurgerConstructorItem from "./burger-constructor-item/burger-constructor-item";
-import { useModal } from "../../hooks/use-modal";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  burgerAddIngredient,
-  burgerClearIngredients,
-  burgerRemoveIngredient,
-  burgerSwapIngredients,
-} from "../../services/reducers/burger";
-import {
-  ingredientCounterDec,
-  ingredientCounterInc,
-  ingredientsCountersReset,
-} from "../../services/reducers/ingredients";
-import { orderCreate } from "../../services/reducers/order";
-import { TDispatch, TRootState } from "../../services/store";
+import { useEffect, useMemo, useRef } from "react"
+import { useDrop } from "react-dnd"
+import { nanoid } from "nanoid"
+import { TBurger, TIngredient } from "../../types"
+import { ConstructorElement, CurrencyIcon, Button } from "@ya.praktikum/react-developer-burger-ui-components"
+import Modal from "../modal/modal"
+import OrderDetails from "../order-details/order-details"
+import styles from "./burger-constructor.module.css"
+import BurgerConstructorItem from "./burger-constructor-item/burger-constructor-item"
+import { useModal } from "../../hooks/use-modal"
+import { useDispatch, useSelector } from "react-redux"
+import { burgerAddIngredient, burgerClearIngredients, burgerRemoveIngredient, burgerSwapIngredients } from "../../services/reducers/burger"
+import { ingredientCounterDec, ingredientCounterInc, ingredientsCountersReset } from "../../services/reducers/ingredients"
+import { orderCreate } from "../../services/reducers/order"
+import { TDispatch, TRootState } from "../../services/store"
 
 const BurgerConstructor = () => {
-  const dispatch = useDispatch<TDispatch>();
+  const dispatch = useDispatch<TDispatch>()
 
-  const burger: TBurger = useSelector((store: TRootState) => store.burger);
-  const loading = useSelector((store: TRootState) => store.order.loading);
+  const burger: TBurger = useSelector((store: TRootState) => store.burger)
+  const { loading, success } = useSelector((store: TRootState) => store.order)
 
-  const { isModalOpen, openModal, closeModal } = useModal(false);
+  const { isModalOpen, openModal, closeModal } = useModal(false)
 
-  const scrolledWindow = useRef<HTMLDivElement>(null);
+  const scrolledWindow = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    onResize();
-    window.addEventListener("resize", onResize);
+    onResize()
+    window.addEventListener("resize", onResize)
 
     return () => {
-      window.removeEventListener("resize", onResize);
-    };
-  }, []);
+      window.removeEventListener("resize", onResize)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (success) {
+      dispatch(burgerClearIngredients())
+      dispatch(ingredientsCountersReset())
+    }
+  }, [success, dispatch])
 
   const onResize = () => {
-    const maxHeight = Math.ceil((window.innerHeight - 600) / 96) * 96;
-    (scrolledWindow.current as HTMLElement).style.maxHeight = `${
-      maxHeight < 96 ? 96 : maxHeight
-    }px`;
-  };
+    const maxHeight = Math.ceil((window.innerHeight - 600) / 96) * 96
+    ;(scrolledWindow.current as HTMLElement).style.maxHeight = `${maxHeight < 96 ? 96 : maxHeight}px`
+  }
 
   const [, dropIngredient] = useDrop({
     accept: "ingredient",
     drop: (item: { [key: string]: TIngredient }) => {
       if (item.ingredient.type === "bun" && burger.bun) {
-        dispatch(ingredientCounterDec(burger.bun._id));
+        dispatch(ingredientCounterDec(burger.bun._id))
       }
-      dispatch(burgerAddIngredient({ ...item.ingredient, id: nanoid() }));
-      dispatch(ingredientCounterInc(item.ingredient._id));
-    },
-  });
+      dispatch(burgerAddIngredient({ ...item.ingredient, id: nanoid() }))
+      dispatch(ingredientCounterInc(item.ingredient._id))
+    }
+  })
 
-  const burgerEmpty = useMemo(
-    () => !(burger.bun !== null || burger.filling.length > 0),
-    [burger]
-  );
+  const burgerEmpty = useMemo(() => !(burger.bun !== null || burger.filling.length > 0), [burger])
 
-  const burgerPrice = useMemo(
-    () =>
-      !burgerEmpty
-        ? burger.filling.reduce(
-            (acc, item) => acc + item.price,
-            burger.bun ? burger.bun.price * 2 : 0
-          )
-        : 0,
-    [burger, burgerEmpty]
-  );
+  const burgerPrice = useMemo(() => (!burgerEmpty ? burger.filling.reduce((acc, item) => acc + item.price, burger.bun ? burger.bun.price * 2 : 0) : 0), [burger, burgerEmpty])
 
   const deleteIngredient = (ingredientId: string, tmpId: string) => {
-    dispatch(burgerRemoveIngredient(tmpId));
-    dispatch(ingredientCounterDec(ingredientId));
-  };
+    dispatch(burgerRemoveIngredient(tmpId))
+    dispatch(ingredientCounterDec(ingredientId))
+  }
 
   const swapIngredients = (first: number, second: number) => {
-    dispatch(burgerSwapIngredients({ first, second }));
-  };
+    dispatch(burgerSwapIngredients({ first, second }))
+  }
 
   const createOrder = () => {
-    let tmp = burger.bun ? [burger.bun._id, burger.bun._id] : [];
-    tmp = [...tmp, ...burger.filling.map((item) => item._id)];
+    let tmp = burger.bun ? [burger.bun._id, burger.bun._id] : []
+    tmp = [...tmp, ...burger.filling.map((item) => item._id)]
 
-    dispatch(orderCreate(tmp));
-    dispatch(burgerClearIngredients());
-    dispatch(ingredientsCountersReset());
-    openModal();
-  };
+    dispatch(orderCreate(tmp))
+    openModal()
+  }
 
   return (
     <section className={styles.section} ref={dropIngredient}>
@@ -105,53 +83,26 @@ const BurgerConstructor = () => {
         {burger.bun && (
           <div className={styles.item}>
             <div className={styles.drag}></div>
-            <ConstructorElement
-              type="top"
-              isLocked={true}
-              text={`${burger.bun.name} (верх)`}
-              price={burger.bun.price}
-              thumbnail={burger.bun.image}
-            />
+            <ConstructorElement type='top' isLocked={true} text={`${burger.bun.name} (верх)`} price={burger.bun.price} thumbnail={burger.bun.image} />
           </div>
         )}
         <div className={styles.scrolled} ref={scrolledWindow}>
           {burger.filling.map((item, index) => (
-            <BurgerConstructorItem
-              ingredient={item}
-              onDelete={deleteIngredient}
-              index={index}
-              key={index}
-              onSwap={swapIngredients}
-            />
+            <BurgerConstructorItem ingredient={item} onDelete={deleteIngredient} index={index} key={index} onSwap={swapIngredients} />
           ))}
         </div>
         {burger.bun && (
           <div className={styles.item}>
             <div className={styles.drag}></div>
-            <ConstructorElement
-              type="bottom"
-              isLocked={true}
-              text={`${burger.bun.name} (низ)`}
-              price={burger.bun.price}
-              thumbnail={burger.bun.image}
-            />
+            <ConstructorElement type='bottom' isLocked={true} text={`${burger.bun.name} (низ)`} price={burger.bun.price} thumbnail={burger.bun.image} />
           </div>
         )}
       </div>
       {!burgerEmpty && (
         <div className={styles.footer}>
-          <span className="text text_type_digits-medium mr-2">
-            {burgerPrice}
-          </span>
-          <CurrencyIcon type="primary" />
-          <Button
-            htmlType="button"
-            type="primary"
-            size="medium"
-            extraClass="ml-10"
-            onClick={createOrder}
-            disabled={loading}
-          >
+          <span className='text text_type_digits-medium mr-2'>{burgerPrice}</span>
+          <CurrencyIcon type='primary' />
+          <Button htmlType='button' type='primary' size='medium' extraClass='ml-10' onClick={createOrder} disabled={loading}>
             Оформить заказ
           </Button>
         </div>
@@ -162,7 +113,7 @@ const BurgerConstructor = () => {
         </Modal>
       )}
     </section>
-  );
-};
+  )
+}
 
-export default BurgerConstructor;
+export default BurgerConstructor
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,44 +1,38 @@
-const API = "https://norma.nomoreparties.space/api";
+const API = "https://norma.nomoreparties.space/api"
 
 type TResponse = {
-  [key: string]: unknown;
-};
+  [key: string]: unknown
+}
 
 class _Api {
-  private _baseUrl = API;
+  private _baseUrl = API
 
   private _checkResponse = (response: Response) => {
-    return response.ok
-      ? response.json()
-      : Promise.reject(`Error ${response.status}`);
-  };
+    return response.ok ? response.json() : Promise.reject(`Error ${response.status}`)
+  }
 
   private _checkSuccess = (response: TResponse) => {
-    return response.success
-      ? Promise.resolve(response)
-      : Promise.reject(`Response not success ${response}`);
-  };
+    return response.success ? Promise.resolve(response) : Promise.reject(`Response not success ${response}`)
+  }
 
   private _request = (url: string, options = {}) => {
-    return fetch(`${this._baseUrl}${url}`, options)
-      .then(this._checkResponse)
-      .then(this._checkSuccess);
-  };
+    return fetch(`${this._baseUrl}${url}`, options).then(this._checkResponse).then(this._checkSuccess)
+  }
 
   public loadIngredients = () => {
-    return this._request("/ingredients");
-  };
+    return this._request("/ingredients")
+  }
 
   public createOrder = (ingredients: string[]) => {
     return this._request("/orders", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify({ ingredients }),
-    });
-  };
+      body: JSON.stringify({ ingredients })
+    })
+  }
 }
 
-const Api = new _Api();
-export default Api;
+const Api = new _Api()
+export default Api
