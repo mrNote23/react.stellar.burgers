970b3b710aa21d33a35b4df5a8edf248fb6890f5
AndreyS <suvorov.av93@gmail.com>
2023-06-19 16:35:37 +0300

    feat: constants definitions

---
+++ src/config/constants.ts
@@ -0,0 +1,11 @@
+export const ACCESS_TOKEN_NAME = "accessToken";
+export const REFRESH_TOKEN_NAME = "refreshToken";
+
+export const ACCESS_COOKIE_OPTIONS = {
+  expires: 20 * 60,
+};
+
+export const RESET_PASSWORD_COOKIE_NAME = "resettingPassword";
+export const RESET_PASSWORD_COOKIE_OPTIONS = {
+  expires: 1 * 60 * 60,
+};
