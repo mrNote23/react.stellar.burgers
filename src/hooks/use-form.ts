b37e62a38bfbee8a06333131e819af05bc769e98
AndreyS <suvorov.av93@gmail.com>
2023-06-19 16:35:59 +0300

    feat: useForm hook

--- src/hooks/use-form.ts
+++ src/hooks/use-form.ts
@@ -1,14 +1,21 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 export const useForm = <T>(initialFields: T) => {
   const [form, setForm] = useState(initialFields);
+  const [modified, setModified] = useState(false);
+
+  useEffect(() => {
+    setModified(JSON.stringify(form) !== JSON.stringify(initialFields));
+  }, [form, initialFields]);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setForm((prev) => ({ ...prev, [name]: value }));
   };
 
-  const resetForm = () => setForm(initialFields);
+  const resetForm = () => {
+    setForm(initialFields);
+  };
 
-  return { form, setForm, onChange, resetForm };
+  return { form, setForm, onChange, resetForm, modified };
 };
