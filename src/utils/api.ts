4ab80d79e3e7775689271fa4609c7e520b75a7ec
AndreyS <suvorov.av93@gmail.com>
2023-06-19 16:38:37 +0300

    feat: user methods & jwt service

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,38 +1,159 @@
-const API = "https://norma.nomoreparties.space/api"
+import { TUserLogin, TUserRegister } from "../config/types";
+import { deleteCookie, getCookie, setCookie } from "./cookie";
+import {
+  ACCESS_COOKIE_OPTIONS,
+  ACCESS_TOKEN_NAME,
+  REFRESH_TOKEN_NAME,
+} from "../config/constants";
+
+const API = "https://norma.nomoreparties.space/api";
 
 type TResponse = {
-  [key: string]: unknown
-}
+  [key: string]: unknown;
+};
 
 class _Api {
-  private _baseUrl = API
+  private _baseUrl = API;
+
+  private _checkAuth = async (
+    response: Response | Promise<Response>,
+    url: string,
+    options: {}
+  ) => {
+    if (
+      (response as Response).status >= 400 &&
+      localStorage.getItem(REFRESH_TOKEN_NAME)
+    ) {
+      await this._refreshToken()
+        .then(() => {
+          response = this._fetch(url, options);
+        })
+        .catch(() => {
+          response = Promise.reject("Not authorized");
+        });
+    }
+    return response;
+  };
 
   private _checkResponse = (response: Response) => {
-    return response.ok ? response.json() : Promise.reject(`Error ${response.status}`)
-  }
+    if (response.ok) {
+      return response.json();
+    } else {
+      return response.json().then((err) => Promise.reject(err.message));
+    }
+  };
 
   private _checkSuccess = (response: TResponse) => {
-    return response.success ? Promise.resolve(response) : Promise.reject(`Response not success ${response}`)
-  }
+    return response.success
+      ? Promise.resolve(response)
+      : Promise.reject(`Response not success ${response}`);
+  };
+
+  private _fetch = (url: string, options: {}) => {
+    const headers: { [key: string]: string } = {
+      "Content-Type": "application/json",
+    };
+    const accessToken = getCookie(ACCESS_TOKEN_NAME);
+    if (accessToken) {
+      headers["Authorization"] = accessToken;
+    }
+    return fetch(`${this._baseUrl}${url}` as string, {
+      ...options,
+      headers,
+    });
+  };
 
   private _request = (url: string, options = {}) => {
-    return fetch(`${this._baseUrl}${url}`, options).then(this._checkResponse).then(this._checkSuccess)
-  }
+    return this._fetch(url, options)
+      .then((response) => this._checkAuth(response, url, options))
+      .then(this._checkResponse)
+      .then(this._checkSuccess);
+  };
+
+  private _refreshToken = async () => {
+    await fetch(`${this._baseUrl}/auth/token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ token: localStorage.getItem(REFRESH_TOKEN_NAME) }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          setCookie(ACCESS_TOKEN_NAME, data.accessToken, ACCESS_COOKIE_OPTIONS);
+          localStorage.setItem(REFRESH_TOKEN_NAME, data.refreshToken);
+          return Promise.resolve();
+        } else {
+          return Promise.reject();
+        }
+      })
+      .catch(() => {
+        deleteCookie(ACCESS_TOKEN_NAME);
+        localStorage.removeItem(REFRESH_TOKEN_NAME);
+        return Promise.reject();
+      });
+  };
 
   public loadIngredients = () => {
-    return this._request("/ingredients")
-  }
+    return this._request("/ingredients");
+  };
 
   public createOrder = (ingredients: string[]) => {
     return this._request("/orders", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ ingredients })
-    })
-  }
+      body: JSON.stringify({ ingredients }),
+    });
+  };
+
+  public userLogin = (data: TUserLogin) => {
+    return this._request("/auth/login", {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  };
+
+  public userRegister = (data: TUserRegister) => {
+    return this._request("/auth/register", {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  };
+
+  public userResetPassword = (email: string) => {
+    return this._request("/password-reset", {
+      method: "POST",
+      body: JSON.stringify({ email }),
+    });
+  };
+
+  public userSetPassword = (token: string, password: string) => {
+    return this._request("/password-reset/reset", {
+      method: "POST",
+      body: JSON.stringify({ token, password }),
+    });
+  };
+
+  public userLogout = (token: string) => {
+    return this._request("/auth/logout", {
+      method: "POST",
+      body: JSON.stringify({ token }),
+    });
+  };
+
+  public userAuthorize = () => {
+    return this._request("/auth/user", {
+      method: "GET",
+    });
+  };
+
+  public userUpdateProfile = (data: TUserRegister) => {
+    return this._request("/auth/user", {
+      method: "PATCH",
+      body: JSON.stringify(data),
+    });
+  };
 }
 
-const Api = new _Api()
-export default Api
+const Api = new _Api();
+export default Api;
